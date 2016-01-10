d9ba496887d546effee7c68d62fbbe890caa0514
git <kontakt@bartoszzawadka.pl>
2016-01-10 13:02:53 +0100

    Merged remove-isssue

--- public/javascripts/angular/issuescontroller.js
+++ public/javascripts/angular/issuescontroller.js
@@ -116,15 +116,6 @@ function IssuesCtrl($scope, $location, $mdDialog, $http, $route){
                 title: "Odnośnik",
                 template: "<a href='#= link #' target='_blank'>#= link #</a>"
             }]
-            //},{
-            //    field: "createdAt",
-            //    title: "Data utworzenia",
-            //    format: "{0:yyyy-MM-dd}"
-            //},{
-            //    field: "updatedAt",
-            //    title: "Data modyfikacji",
-            //    format: "{0:yyyy-MM-dd}"
-            //}]
         });
     }
 
